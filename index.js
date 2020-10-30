13ef8170c86c2abe4bcde06da742e54d8d337a69
MehrdadHajebi <mehrdad.h.exe@gmail.com>
2020-10-30 12:10:48 +0330

    Start

---
+++ index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var express          = require('express'),
+    ZarinpalCheckout = require('zarinpal-checkout'),
+    bodyParser       = require('body-parser'),
+    app              = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+/**
+ * Initial ZarinPal module.
+ * @param {String} 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' [MerchantID]
+ * @param {bool} false [toggle `Sandbox` mode]
+ */
+var zarinpal = ZarinpalCheckout.create('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', true);
+
+/**
+ * Route: PaymentRequest [module]
+ * @return {String} URL [Payement Authority]
+ */
+app.get('/PaymentRequest', function(req, res) {
+    zarinpal.PaymentRequest({
+        Amount: '1000',
+        CallbackURL: 'http://siamak.us',
+        Description: 'Hello NodeJS API.',
+        Email: 'hi@siamak.work',
+        Mobile: '09120000000'
+    }).then(function (response) {
+        if (response.status == 100) {
+            res.redirect(response.url);
+        }
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
+/**
+ * Route: PaymentVerification [module]
+ * @return {number} RefID [Check Paymenet state]
+ */
+app.get('/PaymentVerification/:amount/:token', function(req, res) {
+    zarinpal.PaymentVerification({
+        Amount: req.params.amount,
+        Authority: req.params.token,
+    }).then(function (response) {
+        if (response.status == 101) {
+            console.log("Verified! Ref ID: " + response.RefID);
+        } else {
+            console.log(response);
+        }
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
+/**
+ * Route: UnverifiedTransactions [module]
+ * @return {Object} authorities [List of Unverified transactions]
+ */
+app.get('/UnverifiedTransactions', function(req, res) {
+    zarinpal.UnverifiedTransactions().then(function (response) {
+        if (response.status == 100) {
+            console.log(response.authorities);
+        }
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
+/**
+ * Route: Refresh Authority [module]
+ * @param {number} expire [(1800 / 60) = 30min]
+ * @return {String} status [Status of Authority]
+ */
+app.get('/RefreshAuthority/:expire/:token', function(req, res) {
+    zarinpal.RefreshAuthority({
+        Authority: req.params.token,
+        Expire: req.params.expire
+    }).then(function (response) {
+        if (response.status == 100) {
+            res.send('<h2>You can Use: <u>' + req.params.token + '</u> — Expire in: <u>' + req.params.expire + '</u></h2>');
+        }
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
+/**
+ * Serve App on 9990
+ */
+app.listen(9990, function() {
+    console.log('App is Running on Port 9990.');
+});
